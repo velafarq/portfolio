6d235824d7152fab8b5b5bf013661a6af3bf4430
Vela Farquharson <vela@bleachr.co>
2020-08-14 14:13:30 -0500

    Remove tools from home

--- js/home.template.js
+++ js/home.template.js
@@ -3,9 +3,9 @@ const home_template = $(`
 <h2 class="home__title">Vela Farquharson</h2>
 
 <img class="profile-pic" src="./assets/vela-farquharson.JPG" alt="Vela Farquharson"/>  
-<h3 class="home__occupation">Software Developer</h3>
-<h4 class="home__tools"> React | Redux | Node.js | Express | MongoDB</h4>
+<h3 class="home__occupation">Software Developer & Cellist</h3>
         <p class="home__location">Minneapolis, MN</p>
   
    
 </section>`);
+// <h4 class="home__tools"> React | Redux | Node.js | Express | MongoDB</h4>
